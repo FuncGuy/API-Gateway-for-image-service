7aa4c5715d888070bf165886238697fa380fbbfb
FuncGuy <tim25659@gmail.com>
2020-01-05 21:52:55 +0530

    Initial commit

---
+++ src/test/java/com/apigateway/imageservice/ApiGatewayForImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayForImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
